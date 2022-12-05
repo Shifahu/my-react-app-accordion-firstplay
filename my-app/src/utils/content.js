5d5acd50f829d469786dedec785c0b9eb273ab5e
Shifahu <84721409+Shifahu@users.noreply.github.com>
2022-12-05 18:32:41 +0000

    new updates

--- my-app/src/utils/content.js
+++ my-app/src/utils/content.js
@@ -1,26 +1,32 @@
 export const accordionData = [
-    {
-      title: 'About',
-      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+  {
+    title: 'About',
+    content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
       laborum cupiditate possimus labore, hic temporibus velit dicta earum
       suscipit commodi eum enim atque at? Et perspiciatis dolore iure
       voluptatem.`
-    },
-    {
-      title: 'Services',
-      content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
+  },
+  {
+    title: 'Services',
+    content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
       reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
       quaerat iure quos dolorum accusantium ducimus in illum vero commodi
       pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
       quidem maiores doloremque est numquam praesentium eos voluptatem amet!
       Repudiandae, mollitia id reprehenderit a ab odit!`
-    },
-    {
-      title: 'Clients',
-      content: `Dayor, Gabzy, LeMav`
-    },
-    {
-      title: 'Contact',
-      content: `admin@firstplaymt.com`
-    }
-  ];
+  },
+  {
+    title: 'Clients',
+    content: [
+      { client: `Dayor`,
+    link:`https://instagram.com/dayorartist?igshid=YmMyMTA2M2Y=`  },
+      { client: `Gabzy`,
+    link: `https://instagram.com/itsgabzy?igshid=YmMyMTA2M2Y=` },
+      { client: `LeMav`,
+    link: `https://instagram.com/lemav_?igshid=YmMyMTA2M2Y=` }]
+  },
+  {
+    title: 'Contact',
+    content: `admin@firstplaymt.com`
+  }
+];
